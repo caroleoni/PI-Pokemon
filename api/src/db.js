46e583163c0bccad96cb259e44fb1b6ef3178801
caroleoni <carolinaleoni106@gmail.com>
2023-08-07 16:23:14 -0300

    Changes for the deploy

--- api/src/db.js
+++ api/src/db.js
@@ -215,3 +215,6 @@ module.exports = {
 //   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
 //   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 // };
+
+
+// "start": "nodemon -L",
